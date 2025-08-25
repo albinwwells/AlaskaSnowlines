84e9bc76879b617a2cd16b409f7d5f6bc89bb22f
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-08-25 14:48:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,14 +32,13 @@ def load_glaciers(url):
     return gdf
 
 # Center map
-gdf_proj = gdf.to_crs(epsg=3338)
-center = [gdf_proj.geometry.centroid.y.mean(), gdf_proj.geometry.centroid.x.mean()]
+gdf_proj = gdf.to_crs(epsg=3338)  # Alaska Albers
+centroid = gdf_proj.geometry.centroid.unary_union.centroid
+center = gpd.GeoSeries([centroid], crs=gdf_proj.crs).to_crs(epsg=4326)
+center_coords = [center.y.iloc[0], center.x.iloc[0]]
 
-center_lonlat = gpd.GeoSeries([gdf_proj.geometry.centroid.unary_union.centroid], crs=gdf_proj.crs).to_crs(epsg=4326)
-center = [center_lonlat.y.iloc[0], center_lonlat.x.iloc[0]] # get center in lat, lon
-
-# Create folium map
-m = folium.Map(location=center, zoom_start=4, tiles="CartoDB positron")
+# Create map
+m = folium.Map(location=center_coords, zoom_start=4, tiles="CartoDB positron")
 
 # ---------------- Add glacier polygons with popup ----------------
 folium.GeoJson(
