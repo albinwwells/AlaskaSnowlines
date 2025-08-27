b7c497bb42c3be94ed27e75cce87e7a9925e2d7c
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-08-27 05:16:46 -0400

    Update plot_gif.py

--- pages/plot_gif.py
+++ pages/plot_gif.py
@@ -91,6 +91,6 @@ else:
                 with zf.open(fname) as f:
                     html_content = f.read().decode()
                 st.write('pathrow: '+ fname.split(rgi_no+"_")[1].split("_animation")[0])
-                st.components.v1.html(html_content, height=800, scrolling=False)
+                st.components.v1.html(html_content, height=800, scrolling=True)
         else:
             st.error(f"No animation available for {rgi_no} Glacier.")
