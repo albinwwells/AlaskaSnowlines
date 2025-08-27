4f6442b7d8a2cb5a4feb1c8529bf86c8d41f8d49
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-08-27 13:34:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,32 +70,17 @@ if st.session_state.get("current_page") == "map":
     
     with st.spinner("Plotting glaciers..."):
         # ---------------- Map ----------------
-        # bounds = gdf.total_bounds
-        # # center = [(bounds[1] + bounds[3]) / 2, (bounds[0] + bounds[2]) / 2]
-        # center = [61.0, -146.0]
+        bounds = gdf.total_bounds
+        # center = [(bounds[1] + bounds[3]) / 2, (bounds[0] + bounds[2]) / 2]
+        center = [61.0, -146.0]
     
-        # m = folium.Map(location=center, zoom_start=4, tiles="CartoDB positron")
+        m = folium.Map(location=center, zoom_start=4, tiles="CartoDB positron")
     
-        # # ---------------- Add outlines ----------------
-        # folium.GeoJson(
-        #     gdf, # outline_gdf
-        #     style_function=lambda x: {"color": "blue", "weight": 0.5, "fillOpacity": 0.1}
-        # ).add_to(m)
-        @st.cache_resource
-        def build_map(gdf):
-            bounds = gdf.total_bounds
-            center = [61.0, -146.0]
-        
-            m = folium.Map(location=center, zoom_start=4, tiles="CartoDB positron")
-            folium.GeoJson(
-                gdf,
-                style_function=lambda x: {"color": "blue", "weight": 0.5, "fillOpacity": 0.1}
-            ).add_to(m)
-            folium.LayerControl().add_to(m)
-            return m
-        
-        with st.spinner("Plotting glaciers..."):
-            m = build_map(gdf)    
+        # ---------------- Add outlines ----------------
+        folium.GeoJson(
+            gdf, # outline_gdf
+            style_function=lambda x: {"color": "blue", "weight": 0.5, "fillOpacity": 0.1}
+        ).add_to(m)  
     
         # ---------------- Add clickable centroids ----------------
         for _, row in gdf.iterrows():
