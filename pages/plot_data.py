bd2c7867637231690bc06af7c638f24ccafb3f03
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-08-27 01:39:55 -0400

    Update plot_data.py

--- pages/plot_data.py
+++ pages/plot_data.py
@@ -13,57 +13,6 @@ st.set_page_config(
 st.session_state["current_page"] = "plot"
 
 # ---------------- plotting functions ----------------
-# def plot_db_heatmap(db_bin, dates, bins_center, binned_area, set_ymin, set_ymax, glacno,
-#                     cmap='RdYlBu', cbar_label='db', ylabel='Elevation [m a.s.l.]', glac_name_dict={}, figsize=(9,6),
-#                     bins2plot_lowerquantile=2, bins2plot_upperquantile=98, frame_cut=0, title_info='', **kwargs):
-#     """Heatmap plotting function for Streamlit."""
-    
-#     fig, ax = plt.subplots(figsize=figsize)
-    
-#     # interpolate to 12-day intervals
-#     dates_12d = pd.date_range(dates[frame_cut], dates[-1], freq='12D')
-#     db_bin_12d = np.full((db_bin.shape[0], len(dates_12d)), np.nan)
-    
-#     for ndate, date in enumerate(dates_12d):
-#         if date in dates:
-#             idx = np.where(dates == date)[0][0]
-#             db_bin_12d[:, ndate] = db_bin[:, idx]
-
-#     # set color scale
-#     dbmin = np.nanpercentile(db_bin, bins2plot_lowerquantile)
-#     dbmax = np.nanpercentile(db_bin, bins2plot_upperquantile)
-
-#     bin_sizes = np.diff(bins_center)
-#     bin_halfsize = bin_sizes[0]/2
-#     if ylabel == 'Elevation [m a.s.l.]':
-#         assert np.all(bin_sizes == bin_sizes[0]), "Elevation bins not regularly spaced."
-
-#     # plot heatmap
-#     cax = ax.imshow(
-#         db_bin_12d, cmap=cmap, vmin=dbmin, vmax=dbmax,
-#         interpolation='nearest', aspect='auto', origin='lower',
-#         extent=[dates_12d[0], dates_12d[-1], set_ymin, set_ymax]
-#     )
-
-#     # optional additional line plots
-#     for data in kwargs.get('line_plot', []):
-#         x, y, c, ls, lw, label = data
-#         ax.plot(x, y, c=c, ls=ls, lw=lw, label=label)
-#     if kwargs.get('line_plot'):
-#         ax.legend(loc='lower right')
-
-#     # title
-#     glac_name = glac_name_dict.get(glacno, str(glacno))
-#     ax.set_title(glac_name + title_info)
-#     ax.set_ylabel(ylabel)
-#     ax.set_xlim([dates_12d[0], dates_12d[-1]])
-#     ax.set_ylim([set_ymin, set_ymax])
-
-#     # colorbar
-#     fig.colorbar(cax, orientation='vertical', label=cbar_label)
-
-#     # display
-#     st.pyplot(fig)
 def plot_db_heatmap(db_bin, dates, bins_center, binned_area, set_ymin, set_ymax, glacno, cmap='RdYlBu', cbar_label='db', 
                     ylabel='Elevation [m a.s.l.]', glac_name_dict={}, figsize=(9,6), bins2plot_lowerquantile=2, 
                     bins2plot_upperquantile=98, frame_cut=0, title_info='', **kwargs):
@@ -138,7 +87,7 @@ def fetch_snowline_data(rgi_no: str):
     with zipfile.ZipFile(io.BytesIO(response.content)) as zf:
         file_name = f"{rgi_no}.zip"
         if file_name not in zf.namelist():
-            return None, None, None, None
+            return None, None, None, None, None
 
         # Extract inner ZIP as bytes
         with zf.open(file_name) as inner_zip_file:
@@ -149,39 +98,9 @@ def fetch_snowline_data(rgi_no: str):
                         me_list.append(gzf.read(fname.replace("snowline", "melt_extent")).decode())
                         db_list.append(gzf.read(fname.replace("snowline_elev_percentile", "db_bin_mean")).decode())
                         hyps_list.append(gzf.read(fname.replace("snowline_elev_percentile", "hypsometry")).decode())
+                        pr_list.append(gzf.read(fname.split("_snowline_elev_percentile_")[-1]).decode())
 
-    return sl_list, me_list, db_list, hyps_list
-            
-# def fetch_snowline_data(rgi_no: str):
-#     """Fetch snowline and melt extent CSVs from Zenodo for a given glacier number."""
-#     json_url = "https://zenodo.org/records/16956246/files/rgi_data_links.json?download=1"
-#     response = requests.get(json_url)
-#     response.raise_for_status()
-#     rgi_index = response.json()  # dictionary: rgi_no to zip URL
-#     rgi_key = (rgi_no + ".zip").strip()
-
-#     # Get the URL for the specific glacier
-#     try:
-#         zip_name = rgi_index[rgi_key]
-#     except KeyError:
-#         st.write(f"Key not found: '{rgi_key}'")
-#         return None, None, None, None, None
-        
-#     zip_url = f"https://zenodo.org/records/16956246/files/{zip_name}?download=1"
-
-#     # Download the zip
-#     response = requests.get(zip_url)
-#     response.raise_for_status()
-#     with zipfile.ZipFile(io.BytesIO(response.content)) as gzf:
-#         glac_csvs = gzf.namelist()
-#         st.write(glac_csvs)
-
-#         sl_csvs = [f for f in glac_csvs if "snowline_elev_percentile" in f and "eos_corr" not in f and "eabin" not in f]
-#         me_csvs = [f.replace("snowline", "melt_extent") for f in sl_csvs]
-#         db_csvs = [f.replace("snowline_elev_percentile", "db_bin_mean") for f in sl_csvs]
-#         hyps_csvs = [f.replace("snowline_elev_percentile", "hypsometry") for f in sl_csvs]
-#         return sl_csvs, me_csvs, db_csvs, hyps_csvs
-
+    return sl_list, me_list, db_list, hyps_list, pr_list
 
 # ---------------- Main page ----------------
 gdf = st.session_state.get("gdf", None)
@@ -222,12 +141,12 @@ if rgi_no is None:
 else:
     # Convert RGI ID → RGI number (your convention: 01.xxxxx)
     st.write(f"### Data for RGI v7: {rgi_no}")
-    sl_dfs, me_dfs, db_dfs, hyps_dfs = fetch_snowline_data(rgi_no)
+    sl_dfs, me_dfs, db_dfs, hyps_dfs, prs = fetch_snowline_data(rgi_no)
 
     if sl_dfs is None:
         st.error("No snowline data found for this glacier.")
     else:
-        for sl_df, me_df, db_df, hyps_df in zip(sl_dfs, me_dfs, db_dfs, hyps_dfs):
+        for sl_df, me_df, db_df, hyps_df, pr in zip(sl_dfs, me_dfs, db_dfs, hyps_dfs, prs):
             sl_df = pd.read_csv(io.StringIO(sl_df), index_col=0, parse_dates=True)
             me_df = pd.read_csv(io.StringIO(me_df), index_col=0, parse_dates=True)
             db_df = pd.read_csv(io.StringIO(db_df), index_col=0, parse_dates=True)
@@ -250,7 +169,7 @@ else:
             # ---------------- Plot ----------------
             fig = plot_db_heatmap(db_bin=glac_binned_data,  dates=dates, bins_center=glac_zbins_center,
                                   binned_area=binned_area, set_ymin=set_ymin, set_ymax=set_ymax,
-                                  glacno=rgi_no, figsize=(12, 4), 
+                                  glacno=rgi_no, title_info=f"(pathrow: {pr})", figsize=(12, 4), 
                                   line_plot=[(dates_per, me_elev_per, 'k', '-', 0.7, 'Melt extent'),
                                              (dates_per, sl_elev_per, 'k', '-.', 0.7, 'Snowline')])
             st.pyplot(fig)
