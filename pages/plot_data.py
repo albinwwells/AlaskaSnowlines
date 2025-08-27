70cfe251c191e49b5fe922eccd63fdb3522c708d
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-08-27 02:06:32 -0400

    Update plot_data.py

--- pages/plot_data.py
+++ pages/plot_data.py
@@ -149,7 +149,6 @@ else:
         st.error("No snowline data found for this glacier.")
     else:
         for sl_df, me_df, db_df, hyps_df, pr in zip(sl_dfs, me_dfs, db_dfs, hyps_dfs, prs):
-            st.write(db_df)
             st.write(hyps_df)
             
             sl_df = pd.read_csv(io.StringIO(sl_df), index_col=0, parse_dates=[0], 
