f26afad6b11815875806086c8bb482a97f482a46
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-09-02 00:08:18 -0400

    Update plot_gif.py

--- pages/plot_gif.py
+++ pages/plot_gif.py
@@ -9,6 +9,15 @@ st.set_page_config(
     initial_sidebar_state="collapsed"  # <- hides/collapses sidebar
 )
 
+def nav():
+    with st.sidebar:
+        st.title("Navigation")
+        st.page_link("https://alaskasnowlines.streamlit.app/", label="Home - glacier selection")
+        st.page_link("https://alaskasnowlines.streamlit.app/plot_elev", label="Heatmap - elevation bins")
+        st.page_link("https://alaskasnowlines.streamlit.app/plot_area", label="Heatmap - area bins")
+        st.page_link("https://alaskasnowlines.streamlit.app/plot_gif", label="Glacier animations")
+nav()
+
 # hide_sidebar_style = """
 #     <style>
 #         [data-testid="stSidebar"] {display: none;}
