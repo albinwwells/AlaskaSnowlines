029dbcb938c051bc07713bd8454fc7cf433c96b8
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-09-12 13:38:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown(
             display:inline-block;
             margin-right:5px;
             padding:6px 12px;
-            background:#23415a;
+            background:#315b7e;
             color:white;
             text-decoration:none;
             border-radius:4px;">
@@ -48,7 +48,7 @@ st.markdown(
         <a href="https://alaskasnowlines.streamlit.app/plot_gif" target="_blank" style="
             display:inline-block;
             padding:6px 12px;
-            background:#1c3448;
+            background:#2a4e6c;
             color:white;
             text-decoration:none;
             border-radius:4px;">
