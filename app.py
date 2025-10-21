cca4eeb241f1c4bb2c5213c6175439ba91f5ce5b
Albin Wells <91861031+albinwwells@users.noreply.github.com>
2025-10-21 01:55:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -258,7 +258,7 @@ st.markdown(
 col1, col2, col3 = st.columns([1, 1, 1])
 with col2:
     st.download_button(
-        label="Download regional synthesis files",
+        label="Download regional files (e.g., glacier melt days, melt season start/end, subregion snow-free area fraction)",
         data=reg_zip,
         file_name="regional_me_sl.zip",
         mime="application/zip"
